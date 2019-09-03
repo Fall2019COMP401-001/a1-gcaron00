7c80788fa3ac23e576783678467104a0649ba7ec
gcaron00 <54374820+gcaron00@users.noreply.github.com>
2019-09-02 23:03:20 -0400

    al assignment

--- src/a1/A1Example.java
+++ src/a1/A1Example.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class A1Example {
 
 	public static void main(String[] args) {
+	
 		// Print out the Java version number, just to let 
 		// you know what Eclipse thinks it is using and
 		// so that you can confirm that it is 10.0 or higher.
@@ -17,6 +18,8 @@ public class A1Example {
 		Scanner scan = new Scanner(System.in);
 
 		// Read in count of numbers to process
+		
+
 		
 		int count = scan.nextInt();
 		
--- src/a1/A1Jedi.java
+++ src/a1/A1Jedi.java
@@ -8,7 +8,54 @@ public class A1Jedi {
 		
 		Scanner scan = new Scanner(System.in);
 
-		// Your code follows here.
+		System.out.println("Your Java version: " + System.getProperty("java.version"));
 		
+		int itemcount = scan.nextInt();
+		// number of items sold
+		String[] items = new String[itemcount];
+		double[] costs = new double[itemcount];
+		for(int i=0; i<itemcount; i++) {
+			items[i] = scan.next();
+			costs[i] = scan.nextDouble();
+		}
+		//System.out.println(items);
+		
+		int	peoplecount = scan.nextInt();
+		String[] people = new String[peoplecount];
+		
+		int[] numberofcustomers = new int[itemcount];
+		int[] itemsbought = new int[itemcount];
+		
+		for(int i=0; i<peoplecount; i++) {
+			people[i] = scan.next() + " " + scan.next();
+			int itemspurchased = scan.nextInt();
+			for(int x=0; x<itemspurchased; x++) {
+				int numberofeach = scan.nextInt();
+				String item = scan.next();
+				int y = 0;
+				while(y<itemcount) {
+					String a = items[y];
+					if (a.equals(item)) {
+						numberofcustomers[y]++;
+						itemsbought[y] = itemsbought[y] + numberofeach;
+						y = itemcount;
+						
+					} else {
+						y++;
+					}
+				}
+
+			}
+		}
+		for (int i=0; i<itemcount; i++) {
+			
+			if (numberofcustomers[i]>0) {
+				System.out.println(numberofcustomers[i] + " customers bought " + itemsbought[i] + " " + items[i]);
+			} else {
+				System.out.println("No customers bought" + " " + items[i]);
+			}
+		}	
+			
+		scan.close();
 	}
 }
